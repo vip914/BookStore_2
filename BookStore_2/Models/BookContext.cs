c782d88ee4e4d82e4ac172f42fe39062905ed9b2
Andrew <vip914@me.com>
2017-11-10 17:51:02 +0200

    111

--- BookStore_2/Models/BookContext.cs
+++ BookStore_2/Models/BookContext.cs
@@ -15,16 +15,6 @@ namespace BookStore_2.Models
 
     }
 
-    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
-    {
-        protected override void Seed(BookContext db)
-        {
-            db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
-            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургеньев", Price = 180 });
-            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
-
-            base.Seed(db);
-        }
-    }
+   
 
 }
